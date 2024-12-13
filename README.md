07ae836a0dce553bd192f1c9339f8961fbca6a76
Jason Appiah <114092811+JasonAK19@users.noreply.github.com>
2024-12-13 16:06:36 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,107 @@
-# Simple-Shell
+[![Review Assignment Due Date](https://classroom.github.com/assets/deadline-readme-button-22041afd0340ce965d47ae6ef1cefeee28c7c493a6346c4f15d667ab976d596c.svg)](https://classroom.github.com/a/OHwIpFF9)
+# Project 1
+Answer these questions please:
+
+Project Overview:
+-----------------------------------
+What is the project about? Provide a brief overview that includes the project's name, purpose, and target audience.
+
+This project mimics the debian bash terminal that on the users computer
+
+Solution Approach:
+------------------------------------------------------------
+
+How did you approach the problem? Outline the steps or phases in your project development process?
+I started with breaking down what functions i needed to do first, I decided to first start with the get user command function. With each function i looked up what the suggested functions in the comments did to get a better idea on how to implement them. After I completed part 2 of the project, that was when i first started to use valgrind to check for memory leaks and fix the leaks that did happen. After completing part 3 I added error handling.
+
+Challenges and Solutions?
+---------------------------
+What challenges did you encounter while completeing your project, and how did you overcome them?
+the parse command function was most challenging function to implement for me until i learned about strok() function in office hours which kind of works like .strip() in python
+
+
+LLM Disclosure
+--------------
+Are there any external resources or references that might be helpful?
+
+
+Testing and Validation
+-----------------------
+Testing Methodology: How did you test your project? Discuss any unit tests, integration tests, or user acceptance testing conducted.
+Are there any specific commands or scripts that should be known?
+My testing methodology was basically just entering any command i could think of and seeing the output and comparing it to the output of the actual shell
+
+Example session
+---------------
+Provide examples on running your project
+
+jasonappiah@debian:~/project1-JasonAK19$ ./test
+>> history
+176: history
+177: exit
+178: /proc/cpuinfo
+179: exit
+180: ls
+181: cd
+182: cd
+183: pwd
+184: exit
+185: history
+>> ls
+main_template.c  Makefile  README.md  shelldir  simple_shell.c  test  testdir  utils.c  utils.h
+>> pwd
+/home/jasonappiah/project1-JasonAK19
+>> /proc/cpuinfo
+processor       : 0
+vendor_id       : GenuineIntel
+cpu family      : 6
+model           : 191
+model name      : 13th Gen Intel(R) Core(TM) i7-13700HX
+stepping        : 2
+microcode       : 0xffffffff
+cpu MHz         : 2304.000
+cache size      : 30720 KB
+physical id     : 0
+siblings        : 1
+core id         : 0
+cpu cores       : 1
+apicid          : 0
+initial apicid  : 0
+fpu             : yes
+fpu_exception   : yes
+cpuid level     : 22
+wp              : yes
+flags           : fpu vme de pse tsc msr pae mce cx8 apic sep mtrr pge mca cmov pat pse36 clflush mmx fxsr sse sse2 ht syscall nx rdtscp lm constant_tsc rep_good nopl xtopology nonstop_tsc cpuid tsc_known_freq pni pclmulqdq ssse3 cx16 pcid sse4_1 sse4_2 movbe popcnt aes rdrand hypervisor lahf_lm abm 3dnowprefetch invpcid_single ibrs_enhanced fsgsbase bmi1 bmi2 invpcid rdseed clflushopt arat md_clear flush_l1d arch_capabilities
+bugs            : spectre_v1 spectre_v2 spec_store_bypass swapgs retbleed eibrs_pbrsb
+bogomips        : 4608.00
+clflush size    : 64
+cache_alignment : 64
+address sizes   : 39 bits physical, 48 bits virtual
+power management:
+
+>> exit
+
+jasonappiah@debian:~/project1-JasonAK19$ ./test
+>> ls
+main_template.c  Makefile  README.md  shelldir  simple_shell.c  test  testdir  utils.c  utils.h
+>> 
+>> pwd
+/home/jasonappiah/project1-JasonAK19
+>> sdsadadasdsad
+execution error: No such file or directory
+>> cd testdir
+>> cd ..
+>>  
+>>      
+>> history
+188: /proc/cpuinfo
+189: exit
+190: ls
+191: 
+192: pwd
+193: sdsadadasdsad
+194: cd
+195:  
+196:      
+197: history
+>> exit
---
+++ simple_shell.c
@@ -0,0 +1,339 @@
+/*
+project: 01
+author: Jason Appiah-Kubi
+email: jasona2@umbc.edu
+student id: ON35331
+description: a simple linux shell designed to perform basic linux commands
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <ctype.h>
+#include "utils.h"
+
+#define HISTORY_FILE ".421sh"
+/*
+In this project, you are going to implement a number of functions to 
+create a simple linux shell interface to perform basic linux commands
+*/
+
+void user_prompt_loop();
+char* get_user_command();
+char** parse_command(char* command);
+void execute_command(char** parsed_command);
+void handle_proc(char* path);
+void log_command(char *command);
+void show_history();
+/*extra credit*/
+void change_directory(char **args);
+
+
+int main(int argc, char **argv)
+{
+    /*
+    Write the main function that checks the number of argument passed to ensure 
+    no command-line arguments are passed; if the number of argument is greater 
+    than 1 then exit the shell with a message to stderr and return value of 1
+    otherwise call the user_prompt_loop() function to get user input repeatedly 
+    until the user enters the "exit" command.
+    */
+
+   if (argc > 1) {
+        fprintf(stderr, "Usage: %s\n", argv[0]);
+        return 1;
+    }
+   user_prompt_loop();
+    return 0;
+}
+
+void change_directory(char **args) {
+    if (args[1] == NULL) {
+        //home directory is defualt
+        chdir(getenv("HOME"));
+    } else {
+        if (chdir(args[1]) != 0) {
+            perror("cd");
+        }
+    }
+}
+
+void log_command(char *command){
+    FILE *file = fopen(HISTORY_FILE, "a");
+    fprintf(file, "%s\n", command);
+    fclose(file);
+}
+
+void show_history() {
+    FILE *file = fopen(HISTORY_FILE, "r");
+
+    if (file == NULL) {
+        perror("Failed to open history file");
+        return;
+    }
+
+    char *history[10] = {NULL}; 
+    int count = 0;
+    char *buffer = NULL;
+    size_t len = 0;
+
+    // Read lines from the file
+    while (getline(&buffer, &len, file) != -1) {
+        // Remove newline character
+        size_t length = strlen(buffer);
+        if (length > 0 && buffer[length - 1] == '\n') {
+            buffer[length - 1] = '\0';
+        }
+
+        if (count >= 10) {
+            free(history[count % 10]);
+        }
+
+        history[count % 10] = strdup(buffer);
+        count++;
+    }
+
+    fclose(file);
+    free(buffer);
+
+    // Display the last 10 commands
+    int start = (count > 10) ? count - 10 : 0;
+    for (int i = start; i < count; i++) {
+        printf("%d: %s\n", i + 1, history[i % 10]);
+    }
+
+    // Free memory
+    for (int i = 0; i < (count < 10 ? count : 10); i++) {
+        free(history[i]);
+    }
+}
+
+
+/*
+user_prompt_loop():
+Get the user input using a loop until the user exits, prompting the user for a command.
+Gets command and sends it to a parser, then compares the first element to the two
+different commands ("/proc", and "exit"). If it's none of the commands, 
+send it to the execute_command() function. If the user decides to exit, then exit 0 or exit 
+with the user given value. 
+*/
+
+void user_prompt_loop() {
+    // initialize variables
+
+    /*
+    loop:
+        1. prompt the user to type command by printing >>
+        2. get the user input using get_user_command() function 
+        3. parse the user input using parse_command() function 
+        Example: 
+            user input: "ls -la"
+            parsed output: ["ls", "-la", NULL]
+        4. compare the first element of the parsed output to "/proc"and "exit"
+        5. if the first element is "/proc" then you have the open the /proc file system 
+           to read from it
+            i) concat the full command:
+                Ex: user input >>/proc /process_id_no/status
+                    concated output: /proc/process_id_no/status
+            ii) read from the file line by line. you may user fopen() and getline() functions
+            iii) display the following information according to the user input from /proc
+                a) Get the cpu information if the input is /proc/cpuinfo
+                - Cpu Mhz
+                - Cache size
+                - Cpu cores
+                - Address sizes
+                b) Get the number of currently running processes from /proc/loadavg
+                c) Get how many seconds your box has been up, and how many seconds it has been idle from /proc/uptime
+                d) Get the following information from /proc/process_id_no/status
+                - the vm size of the virtual memory allocated the vbox 
+                - the most memory used vmpeak 
+                - the process state
+                - the parent pid
+                - the number of threads
+                - number of voluntary context switches
+                - number of involuntary context switches
+                e) display the list of environment variables from /proc/process_id_no/environ
+                f) display the performance information if the user input is /proc/process_id_no/sched
+        6. if the first element is "exit" the use the exit() function to terminate the program
+        7. otherwise pass the parsed command to execute_command() function 
+        8. free the allocated memory using the free() function
+    */
+
+    /*
+    Functions you may need: 
+        get_user_command(), parse_command(), execute_command(), strcmp(), strcat(), 
+        strlen(), strncmp(), fopen(), fclose(), getline(), isdigit(), atoi(), fgetc(), 
+        or any other useful functions
+    */
+   while(1){
+    printf(">> ");
+    char *command = get_user_command();
+    char **parsed_command = parse_command(command);
+    log_command(command);
+
+    
+  if (parsed_command[0] != NULL) {
+            if (strcmp(parsed_command[0], "exit") == 0) {
+                free(command);
+                free(parsed_command);
+                exit(0);
+            }
+
+            else if (strncmp(parsed_command[0], "/proc", 5) == 0) {
+            handle_proc(parsed_command[0]);  
+            } 
+            else if(strcmp(parsed_command[0], "history") == 0) {
+            show_history();
+            }if (strcmp(parsed_command[0], "cd") == 0) {
+            change_directory(parsed_command);
+            }else {
+            execute_command(parsed_command);
+            }
+   }
+       else {
+            execute_command(parsed_command);
+            }   
+    free(command);
+    free(parsed_command);
+
+}
+}
+
+/*
+get_user_command():
+Take input of arbitrary size from the user and return to the user_prompt_loop()
+*/
+char* get_user_command()
+{
+   char *command = NULL;
+   size_t size = 0;
+   getline(&command, &size, stdin);
+   
+   size_t length = strlen(command);
+   if (length > 0 && command[length - 1] == '\n') {
+        command[length - 1] = '\0'; }
+        
+    //removes the \n with a space
+    for(size_t i = 0; i < length - 1;i++){
+        if (command[i] == '\\' && command[i + 1] == 'n') {
+           command[i] = ' ';
+           memmove(&command[i + 1], &command[i + 2], length - i - 1);
+           length--;
+       }
+    }
+
+   return command;
+}
+
+/*
+parse_command():
+Take command grabbed from the user and parse appropriately.
+Example: 
+    user input: "ls -la"
+    parsed output: ["ls", "-la", NULL]
+Example: 
+    user input: "echo     hello                     world  "
+    parsed output: ["echo", "hello", "world", NULL]
+*/
+char** parse_command(char* command)
+{
+    /*
+    Functions you may need: 
+        malloc(), realloc(), free(), strlen(), first_unquoted_space(), unescape()
+    */
+
+   int buffer = 128;
+   int position = 0;
+   char **strings = malloc(buffer * sizeof(char*));
+   char *string;
+
+   //command = unescape(command,stderr);
+   char *unescaped_command = unescape(command, stderr);
+
+    if (!strings) {
+        fprintf(stderr, "allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    //Splitting the command at the spaces
+    string = strtok(command, " ");
+    while (string != NULL) {
+        strings[position] = string;
+        position++;
+
+        if (position >= buffer) {
+            buffer += 64;
+            strings = realloc(strings, buffer * sizeof(char*));
+            if (!strings) {
+                fprintf(stderr, "allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+
+        string = strtok(NULL, " ");
+    }
+    strings[position] = NULL;  
+    free(unescaped_command);
+
+    return strings;
+}
+
+/*
+execute_command():
+Execute the parsed command if the commands are neither /proc nor exit;
+fork a process and execute the parsed command inside the child process
+*/
+
+void execute_command(char** parsed_command)
+{
+    /*
+    Functions you may need: 
+        fork(), execvp(), waitpid(), and any other useful function
+    */
+
+    pid_t id = fork();
+    if (id == -1){
+        perror("failed");
+        return;
+    }
+    else if (id == 0){
+        //added because the execution error and exit failure message would be printed when using history or any /proc command
+        if (strcmp(parsed_command[0], "history") == 0 || strncmp(parsed_command[0], "/proc", 5) == 0) {
+            exit(EXIT_SUCCESS); 
+        }
+        // message for when a command cant be executed for whatever reason
+        if(execvp(parsed_command[0], parsed_command) < 0){
+            perror("execution error");
+        }
+        exit(EXIT_FAILURE); 
+    }
+    else{
+        int status;
+        waitpid(id, &status, WUNTRACED);
+    }   
+}
+
+
+/*
+  Function that opens the specified /proc file, reads it line by line, and prints the contents
+*/
+void handle_proc(char *path){
+    FILE *file = fopen(path, "r");
+    if (file == NULL){
+        perror("failed to open file");
+        return;
+    }
+
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    while ((read = getline(&line, &len, file)) != -1){
+        printf("%s", line);
+    }
+    free(line);
+    fclose(file);
+}
+
